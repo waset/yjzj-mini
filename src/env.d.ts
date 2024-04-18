d415cf1b0628e565f78499aa5d2a41bf3942a25c
waset <waset@foxmail.com>
2024-04-18 15:35:17 +0800

    chore(config): vite env

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,12 @@ declare module '*.vue' {
   const component: DefineComponent<object, object, any>
   export default component
 }
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+  // 接口地址
+  readonly VITE_API_URL: string
+}
