fb0b6c19e640d55dc40ac2a4d74bcae61a645665
waset <waset@foxmail.com>
2024-05-07 13:17:20 +0800

    perf(utils): 图标转换工具加载调整

--- vite.config.ts
+++ vite.config.ts
@@ -9,73 +9,79 @@ import UnoCSS from 'unocss/vite'
 import { uniuseAutoImports } from '@uni-helper/uni-use'
 import Modules from 'vite-plugin-use-modules'
 import { uniPolyfill } from './bin/uni-polyfill'
-import { Convert } from './bin/convert-icon'
+
+// convert-icon
+import { Converts } from './bin/convert-icon'
+import { icons as ConvertsOptions } from './project.config'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    // 转换图标
-    Convert('src/assets/icons', 'icons', 'temp/icons/'),
-    Convert('src/assets/svg', 'svg', 'temp/icons/', false),
-    // https://github.com/dcloudio/uni-app/issues/4604
-    uniPolyfill(),
-    // https://github.com/uni-helper/vite-plugin-uni-manifest
-    UniHelperManifest(),
-    // https://github.com/uni-helper/vite-plugin-uni-pages
-    UniHelperPages({
-      dts: 'temp/types/uni-pages.d.ts',
-    }),
-    // https://github.com/uni-helper/vite-plugin-uni-layouts
-    UniHelperLayouts(),
-    // https://github.com/uni-helper/vite-plugin-uni-components
-    UniHelperComponents({
-      dts: 'temp/types/components.d.ts',
-      directoryAsNamespace: true,
-      deep: true,
-    }),
-    // https://github.com/dishait/vite-plugin-use-modules
-    Modules({
-      target: 'src/plugins',
-    }),
-    Uni(),
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'uni-app',
-        'pinia',
-        uniuseAutoImports(),
-      ],
-      packagePresets: [
-        {
-          package: '@vueuse/core',
-          ignore: [
-            // vueuse core 默认禁用
-            'toRef',
-            'toRefs',
-            'toValue',
-            'utils',
-            // 解决 uni-use 冲突
-            'tryOnScopeDispose',
-            'useNetwork',
-            'useOnline',
-            'usePreferredDark',
-            'useStorage',
-            'useStorageAsync',
-          ],
-          cache: false,
-        },
-      ],
-      dts: 'temp/types/auto-imports.d.ts',
-      dirs: [
-        'src/composables/**',
-        'src/stores/**',
-        'src/utils/**',
-      ],
-      vueTemplate: true,
-    }),
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    UnoCSS(),
-  ],
-})
+export default async () => {
+  // convert-icon
+  await Converts(ConvertsOptions)
+
+  // https://unocss.dev
+  return defineConfig({
+    plugins: [
+      // https://github.com/dcloudio/uni-app/issues/4604
+      uniPolyfill(),
+      // https://github.com/uni-helper/vite-plugin-uni-manifest
+      UniHelperManifest(),
+      // https://github.com/uni-helper/vite-plugin-uni-pages
+      UniHelperPages({
+        dts: 'temp/types/uni-pages.d.ts',
+      }),
+      // https://github.com/uni-helper/vite-plugin-uni-layouts
+      UniHelperLayouts(),
+      // https://github.com/uni-helper/vite-plugin-uni-components
+      UniHelperComponents({
+        dts: 'temp/types/components.d.ts',
+        directoryAsNamespace: true,
+        deep: true,
+      }),
+      // https://github.com/dishait/vite-plugin-use-modules
+      Modules({
+        target: 'src/plugins',
+      }),
+      Uni(),
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'uni-app',
+          'pinia',
+          uniuseAutoImports(),
+        ],
+        packagePresets: [
+          {
+            package: '@vueuse/core',
+            ignore: [
+              // vueuse core 默认禁用
+              'toRef',
+              'toRefs',
+              'toValue',
+              'utils',
+              // 解决 uni-use 冲突
+              'tryOnScopeDispose',
+              'useNetwork',
+              'useOnline',
+              'usePreferredDark',
+              'useStorage',
+              'useStorageAsync',
+            ],
+            cache: false,
+          },
+        ],
+        dts: 'temp/types/auto-imports.d.ts',
+        dirs: [
+          'src/composables/**',
+          'src/stores/**',
+          'src/utils/**',
+        ],
+        vueTemplate: true,
+      }),
+      // https://github.com/antfu/unocss
+      // see unocss.config.ts for config
+      UnoCSS(),
+    ],
+  })
+}
