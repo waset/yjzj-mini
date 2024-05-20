4fa88aa40f7e5ce625b2a3326449fde86fe8d268
waset <waset@foxmail.com>
2024-05-20 18:27:07 +0800

    feat: 登录页布局

--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -42,6 +42,8 @@ const navbar_size = useNavbarSize()
 
 <style lang="scss" scoped>
   .body {
+    width: 100%;
+    height: 100%;
     // navbar
     --navbar-padding-left: var(--navbar-left);
     --navbar-padding-right: var(--navbar-right);
---
+++ src/pages/login/index.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+const agreementChecked = ref(false)
+</script>
+
+<template>
+  <div class="login">
+    <image class="bg" :src="StaticUrl('/images/login-bg.png')" mode="scaleToFill" />
+    <div class="logo">
+      <div class="i-svg-logo" />
+    </div>
+    <div class="btn">
+      <div
+        class="gologin" :class="{
+          disable: !agreementChecked,
+        }"
+      >
+        点击登录
+      </div>
+    </div>
+    <div
+      class="agreement" :class="{
+        selected: agreementChecked,
+      }" @click="agreementChecked = !agreementChecked"
+    >
+      <div class="check" value="cb" checked="true">
+        <div v-if="agreementChecked" class="i-icons-correct" />
+      </div>
+      <div class="text">
+        授权登录即表明同意《一剑装机服务及隐私条款》
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .login {
+    @apply flex-center;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    position: relative;
+    z-index: 0;
+
+    .bg {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+    }
+
+    .logo {
+      @apply flex-center;
+      font-size: 200rpx;
+      line-height: 100rpx;
+    }
+
+    .btn {
+      @apply flex-center;
+
+      .gologin {
+        @apply px-[160rpx] py-[32rpx] bg-green c-black rounded-[32rpx];
+        box-shadow: 4px 4px 0px 0px rgba(167, 245, 34, 0.2);
+
+        &.disable {
+          @apply bg-[#8D8D8D] text-[#f5f5f5];
+          box-shadow: 4px 4px 0px 0px rgba(#8D8D8D, 0.2);
+        }
+      }
+    }
+
+    .agreement {
+      @apply flex-center mt-[32rpx];
+
+      .check {
+        @apply flex-center mr-[16rpx] w-[36rpx] h-[36rpx] border-1 border-solid border-[#DEDEDE] rounded-[4rpx];
+      }
+
+      .text {
+        @apply text-[24rpx];
+      }
+
+      &.selected {
+        @apply text-green;
+
+        .check {
+          @apply border-[#a7f522];
+        }
+      }
+    }
+  }
+</style>
+
+<route lang="json">
+{
+  "layout": "full"
+}
+</route>
