2d4560da9d96a43d5a285e2e3d9885d6c8ea4ea1
LdddLi <1653243099@qq.com>
2024-06-06 18:02:02 +0800

    refactor: 修改orderTypes中num的定义，更改为函数；更新订单计数为真实用户订单量

--- src/pages/me/me.vue
+++ src/pages/me/me.vue
@@ -4,8 +4,6 @@ const { userDesc, isRegister, token } = storeToRefs(useUserStore())
 
 const { orders } = storeToRefs(useOrderStore())
 
-const orderNum = ref(orders.value[0].totalNumber)
-
 onShow(async () => {
   if (token.value) {
     await getUserInfo()
@@ -19,17 +17,23 @@ const orderTypes = [
   {
     icon: 'i-icons-payment',
     text: '待付款',
-    num: 9,
+    num: () => {
+      return orders.value?.filter(item => item.status === 1).length || 0
+    },
   },
   {
     icon: 'i-icons-delivery',
     text: '待发货',
-    num: 0,
+    num: () => {
+      return orders.value?.filter(item => item.status === 2).length || 0
+    },
   },
   {
     icon: 'i-icons-receive',
     text: '待收货',
-    num: 9999,
+    num: () => {
+      return orders.value?.filter(item => item.status === 3).length || 0
+    },
   },
 ]
 /**
@@ -105,7 +109,7 @@ function goLogin() {
         </div>
         <div class="more" @click="Jump('/pages/me/form')">
           <span>全部订单</span>
-          <span class="num">{{ orderNum }}</span>
+          <span v-if="orders" class="num">{{ orders.length }}</span>
           <div class="i-icons-right" />
         </div>
       </div>
@@ -116,12 +120,12 @@ function goLogin() {
               <div class="item">
                 <div class="icon">
                   <div :class="item.icon" />
-                  <template v-if="item.num && item.num < 99">
+                  <template v-if="item.num && item.num() < 99">
                     <div class="num">
-                      {{ item.num }}
+                      {{ orders.length }}
                     </div>
                   </template>
-                  <template v-if="item.num && item.num > 99">
+                  <template v-if="item.num && item.num() > 99">
                     <div class="num">
                       ···
                     </div>
@@ -207,235 +211,137 @@ function goLogin() {
 </template>
 
 <style lang="scss" scoped>
-  .personal {
-    padding: 0 32rpx;
+.personal {
+  padding: 0 32rpx;
 
-    .line {
-      width: 100%;
-      height: 2rpx;
-      background-image: linear-gradient(134deg, rgba(190, 190, 190, 0.2), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.2));
-    }
+  .line {
+    width: 100%;
+    height: 2rpx;
+    background-image: linear-gradient(134deg, rgba(190, 190, 190, 0.2), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.2));
+  }
 
-    .user {
-      position: relative;
-      padding: 16rpx 0;
+  .user {
+    position: relative;
+    padding: 16rpx 0;
 
-      .wrap {
-        display: flex;
-        align-items: center;
-        padding: 32rpx 16rpx;
+    .wrap {
+      display: flex;
+      align-items: center;
+      padding: 32rpx 16rpx;
 
-        .avatar {
-          position: relative;
+      .avatar {
+        position: relative;
 
-          .edit {
-            position: absolute;
-            right: 0;
-            bottom: 0;
-            padding: 2rpx;
-            background-image: linear-gradient(134deg, rgba(190, 190, 190, 0.2), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.2));
-            border-radius: 50%;
+        .edit {
+          position: absolute;
+          right: 0;
+          bottom: 0;
+          padding: 2rpx;
+          background-image: linear-gradient(134deg, rgba(190, 190, 190, 0.2), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.2));
+          border-radius: 50%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          .btn {
             display: flex;
             align-items: center;
             justify-content: center;
-
-            .btn {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              width: 64rpx;
-              height: 64rpx;
-              color: #000;
-              border-radius: 50%;
-              background: rgba(#fff, 1);
-            }
-          }
-
-        }
-
-        .info {
-          flex: 1;
-          padding: 0 24rpx;
-
-          .name {
-            font-weight: 600;
-            font-size: 32rpx;
-            color: #F5F5F5;
-            line-height: 40rpx;
-            text-align: left;
-            font-style: normal;
-            text-transform: none;
-          }
-
-          .phone {
-            padding: 16rpx 0;
-          }
-
-          .phone,
-          .public {
-            font-weight: 400;
-            font-size: 24rpx;
-            color: #F5F5F5;
-            line-height: 40rpx;
-            text-align: left;
-            font-style: normal;
-            text-transform: none;
+            width: 64rpx;
+            height: 64rpx;
+            color: #000;
+            border-radius: 50%;
+            background: rgba(#fff, 1);
           }
         }
+
       }
-    }
 
-    .promotion {
-      padding: 16rpx 0;
+      .info {
+        flex: 1;
+        padding: 0 24rpx;
 
-      .wrap {
-        border-radius: 32rpx;
-        padding: 2rpx;
-        background-image: linear-gradient(113.84deg, rgba(255, 255, 255, 0.8) -6.55%, rgba(255, 255, 255, 0.08) 46.47%, rgba(255, 255, 255, 0.8) 92.28%);
-
-        .box {
-          padding: 32rpx;
-          padding-bottom: 56rpx;
-          border-radius: 32rpx;
-          background-color: #000;
-
-          .title {
-            @apply flex-between;
-            background: linear-gradient(150deg, rgba(190, 190, 190, 0) 5.58%, rgba(190, 190, 190, 0.4) 35.3%, rgba(190, 190, 190, 0.4) 65.93%, rgba(190, 190, 190, 0) 92.04%);
-            border-radius: 0rpx 4rpx 4rpx 0rpx;
-            padding: 8rpx;
-            padding-top: 24rpx;
-
-            .left {
-              text-shadow: 8rpx 8rpx 0 rgba(167, 245, 34, 0.2);
-            }
-
-            .right {
-
-              @apply underline underline-offset-3;
-              font-size: 28rpx;
-            }
-          }
-
-          .body {
-            padding-top: 32rpx;
-
-            .items {
-              @apply flex-around;
-
-              .item {
-                flex: 1;
-                text-align: center;
-
-                .number {
-                  .units {
-                    font-size: 20rpx;
-                    color: #BEBEBE;
-                  }
-                }
-
-                .text {
-                  font-weight: 400;
-                  font-size: 24rpx;
-                  color: #BEBEBE;
-                  line-height: 40rpx;
-                  text-align: center;
-                  font-style: normal;
-                  text-transform: none;
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-
-    .order {
-      padding: 16rpx 0;
-      position: relative;
-      z-index: 0;
-
-      .title {
-        @apply flex-between;
-
-        .text {
-          position: relative;
-          padding: 12rpx 24rpx;
-          font-weight: bold;
-          z-index: 1;
-          font-size: 28rpx;
-
-          &::before {
-            content: '';
-            position: absolute;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 120%;
-            background: #000;
-            border-radius: 32rpx 32rpx 0 0;
-            z-index: -1;
-          }
-        }
-
-        .more {
-          font-weight: 400;
-          font-size: 24rpx;
-          color: #BEBEBE;
+        .name {
+          font-weight: 600;
+          font-size: 32rpx;
+          color: #F5F5F5;
           line-height: 40rpx;
-          text-align: center;
+          text-align: left;
           font-style: normal;
           text-transform: none;
+        }
 
-          .num {
-            padding: 0 8rpx;
-          }
+        .phone {
+          padding: 16rpx 0;
+        }
+
+        .phone,
+        .public {
+          font-weight: 400;
+          font-size: 24rpx;
+          color: #F5F5F5;
+          line-height: 40rpx;
+          text-align: left;
+          font-style: normal;
+          text-transform: none;
         }
       }
+    }
+  }
 
-      .body {
-        position: relative;
-        z-index: 1;
+  .promotion {
+    padding: 16rpx 0;
+
+    .wrap {
+      border-radius: 32rpx;
+      padding: 2rpx;
+      background-image: linear-gradient(113.84deg, rgba(255, 255, 255, 0.8) -6.55%, rgba(255, 255, 255, 0.08) 46.47%, rgba(255, 255, 255, 0.8) 92.28%);
+
+      .box {
+        padding: 32rpx;
+        padding-bottom: 56rpx;
         border-radius: 32rpx;
-        padding: 2rpx;
-        background-image: linear-gradient(113.84deg, rgba(255, 255, 255, 0.8) -6.55%, rgba(255, 255, 255, 0.08) 46.47%, rgba(255, 255, 255, 0.8) 92.28%);
+        background-color: #000;
 
-        .wrap {
-          border-radius: 32rpx;
-          background-color: #000;
+        .title {
+          @apply flex-between;
+          background: linear-gradient(150deg, rgba(190, 190, 190, 0) 5.58%, rgba(190, 190, 190, 0.4) 35.3%, rgba(190, 190, 190, 0.4) 65.93%, rgba(190, 190, 190, 0) 92.04%);
+          border-radius: 0rpx 4rpx 4rpx 0rpx;
+          padding: 8rpx;
+          padding-top: 24rpx;
 
-          .types {
+          .left {
+            text-shadow: 8rpx 8rpx 0 rgba(167, 245, 34, 0.2);
+          }
+
+          .right {
+
+            @apply underline underline-offset-3;
+            font-size: 28rpx;
+          }
+        }
+
+        .body {
+          padding-top: 32rpx;
+
+          .items {
             @apply flex-around;
 
             .item {
               flex: 1;
-              @apply flex-center;
-              flex-direction: column;
-              padding: 32rpx;
+              text-align: center;
 
-              .icon {
-                position: relative;
-                font-size: 56rpx;
-
-                .num {
-                  @apply flex-center;
-                  position: absolute;
+              .number {
+                .units {
                   font-size: 20rpx;
-                  width: 36rpx;
-                  height: 36rpx;
-                  top: 9rpx;
-                  right: -9rpx;
-                  border-radius: 50%;
-                  border: 4rpx solid #fff;
-                  background-color: #F53F3FFF;
+                  color: #BEBEBE;
                 }
               }
 
               .text {
-                padding-top: 16rpx;
                 font-weight: 400;
-                font-size: 28rpx;
-                color: #FFFFFF;
+                font-size: 24rpx;
+                color: #BEBEBE;
                 line-height: 40rpx;
                 text-align: center;
                 font-style: normal;
@@ -446,38 +352,136 @@ function goLogin() {
         }
       }
     }
+  }
 
-    .func {
-      padding: 16rpx 0;
+  .order {
+    padding: 16rpx 0;
+    position: relative;
+    z-index: 0;
+
+    .title {
+      @apply flex-between;
+
+      .text {
+        position: relative;
+        padding: 12rpx 24rpx;
+        font-weight: bold;
+        z-index: 1;
+        font-size: 28rpx;
+
+        &::before {
+          content: '';
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 120%;
+          background: #000;
+          border-radius: 32rpx 32rpx 0 0;
+          z-index: -1;
+        }
+      }
+
+      .more {
+        font-weight: 400;
+        font-size: 24rpx;
+        color: #BEBEBE;
+        line-height: 40rpx;
+        text-align: center;
+        font-style: normal;
+        text-transform: none;
+
+        .num {
+          padding: 0 8rpx;
+        }
+      }
+    }
+
+    .body {
+      position: relative;
+      z-index: 1;
+      border-radius: 32rpx;
+      padding: 2rpx;
+      background-image: linear-gradient(113.84deg, rgba(255, 255, 255, 0.8) -6.55%, rgba(255, 255, 255, 0.08) 46.47%, rgba(255, 255, 255, 0.8) 92.28%);
 
       .wrap {
         border-radius: 32rpx;
-        padding: 2rpx;
-        background-image: linear-gradient(113.84deg, rgba(255, 255, 255, 0.8) -6.55%, rgba(255, 255, 255, 0.08) 46.47%, rgba(255, 255, 255, 0.8) 92.28%);
+        background-color: #000;
 
-        .items {
-          padding: 32rpx;
-          border-radius: 32rpx;
-          background-color: #000;
+        .types {
+          @apply flex-around;
 
           .item {
-            @apply flex-between;
-            padding: 24rpx 0;
+            flex: 1;
+            @apply flex-center;
+            flex-direction: column;
+            padding: 32rpx;
 
-            .left {
-              @apply flex-center;
+            .icon {
+              position: relative;
+              font-size: 56rpx;
 
-              .icon {
+              .num {
                 @apply flex-center;
-                font-size: 40rpx;
-                padding-right: 8rpx;
+                position: absolute;
+                font-size: 20rpx;
+                width: 36rpx;
+                height: 36rpx;
+                top: 9rpx;
+                right: -9rpx;
+                border-radius: 50%;
+                border: 4rpx solid #fff;
+                background-color: #F53F3FFF;
               }
+            }
+
+            .text {
+              padding-top: 16rpx;
+              font-weight: 400;
+              font-size: 28rpx;
+              color: #FFFFFF;
+              line-height: 40rpx;
+              text-align: center;
+              font-style: normal;
+              text-transform: none;
             }
           }
         }
       }
     }
   }
+
+  .func {
+    padding: 16rpx 0;
+
+    .wrap {
+      border-radius: 32rpx;
+      padding: 2rpx;
+      background-image: linear-gradient(113.84deg, rgba(255, 255, 255, 0.8) -6.55%, rgba(255, 255, 255, 0.08) 46.47%, rgba(255, 255, 255, 0.8) 92.28%);
+
+      .items {
+        padding: 32rpx;
+        border-radius: 32rpx;
+        background-color: #000;
+
+        .item {
+          @apply flex-between;
+          padding: 24rpx 0;
+
+          .left {
+            @apply flex-center;
+
+            .icon {
+              @apply flex-center;
+              font-size: 40rpx;
+              padding-right: 8rpx;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
 
 <route lang="json">
