cfcfa5edabe29cecad58eb09bf67e9ca50c10a2e
Yangyanwen <m17630707712@163.com>
2024-06-07 17:42:27 +0800

    chore:开发DIY选配时的功能

--- src/components/product/Diy.vue
+++ src/components/product/Diy.vue
@@ -57,11 +57,21 @@ const DiyICons = [
     </div>
     <div class="top">
       <div class="wrap">
-        <div v-for="(item, index) in DiyICons" :key="index" class="line">
-          <div :class="item.icons" class="icon_style" />
+        <div
+          v-for="(item, index) in DiyICons"
+          :key="index"
+          class="line"
+        >
+          <div
+            :class="item.icons"
+            class="icon_style"
+          />
           {{ item.text }}
           <!-- @click="Jump" -->
-          <div class="alter" @click="() => { Jump('/pages/custom/selectList') }">
+          <div
+            class="alter"
+            @click="() => { Jump('/pages/custom/selectList') }"
+          >
             <div class="i-icons-edit alter_icon" />
             改配
           </div>
@@ -87,35 +97,47 @@ const DiyICons = [
       font-weight: 600;
       color: rgba(245, 245, 245, 1);
     }
-    .desc{
+    .desc {
       font-size: 28rpx;
       font-weight: 400;
       line-height: 40rpx;
       margin-top: 16rpx;
     }
-    .line{
+    .line {
       height: 80rpx;
       line-height: 80rpx;
       overflow: hidden;
       margin-bottom: 20rpx;
       box-sizing: border-box;
-      background: linear-gradient(90.04deg, rgba(39, 39, 39, 0) 2.8%, #272727 24.32%, #272727 49.68%, rgba(39, 39, 39, 0) 79.65%);
+      background: linear-gradient(
+        90.04deg,
+        rgba(39, 39, 39, 0) 2.8%,
+        #272727 24.32%,
+        #272727 49.68%,
+        rgba(39, 39, 39, 0) 79.65%
+      );
       border-bottom: 2rpx solid;
-      border-image-source: linear-gradient(98.8deg, rgba(190, 190, 190, 0.1) 5.58%, rgba(190, 190, 190, 0.4) 35.3%, rgba(190, 190, 190, 0.4) 65.93%, rgba(190, 190, 190, 0.1) 92.04%);
+      border-image-source: linear-gradient(
+        98.8deg,
+        rgba(190, 190, 190, 0.1) 5.58%,
+        rgba(190, 190, 190, 0.4) 35.3%,
+        rgba(190, 190, 190, 0.4) 65.93%,
+        rgba(190, 190, 190, 0.1) 92.04%
+      );
       border-image-slice: 1;
       font-size: 28rpx;
       font-weight: 600;
       position: relative;
       padding: 0rpx 16rpx;
 
-      .icon_style{
+      .icon_style {
         width: 44rpx;
         height: 44rpx;
         margin-right: 12rpx;
       }
-      .alter{
+      .alter {
         min-width: 104rpx;
-        height:48rpx;
+        height: 48rpx;
         line-height: 35rpx;
         position: absolute;
         top: 50%;
@@ -124,13 +146,13 @@ const DiyICons = [
         float: right;
         font-size: 24rpx;
         text-align: center;
-        color: #FFFFFF;
+        color: #ffffff;
         border-radius: 4rpx;
-        border: 2rpx solid #A7F522;
-        background: #FFFFFF33;
+        border: 2rpx solid #a7f522;
+        background: #ffffff33;
         padding: 4rpx 14rpx 4rpx 14rpx;
 
-        .alter_icon{
+        .alter_icon {
           font-size: 24rpx;
         }
       }
