98eb063e79a626b816cc6525f73f26b8948df109
LdddLi <1653243099@qq.com>
2024-06-14 17:29:25 +0800

    feat: 修改下拉菜单组件接收的数据类型，并调整下拉菜单在页面中的定位

--- src/components/common/Drop.vue
+++ src/components/common/Drop.vue
@@ -2,37 +2,44 @@
 interface Props {
   show: boolean
   position: 'top' | 'bottom' | 'left' | 'right'
+  list: {
+    name: string
+    label: string
+  }[]
 }
 
 const props = withDefaults(defineProps<Props>(), {
   position: 'bottom',
 })
 
-const drop = ref<HTMLElement | null>(null)
-
-watchEffect(() => {
-  if (props.show) {
-    console.log(drop.value)
-  }
-})
+const emits = defineEmits<{
+  click: [name: Props['list'][0]['name']]
+}>()
 </script>
 
 <template>
-  <div ref="drop" class="drop">
+  <div
+    class="drop" :style="{
+      '--top': props.position === 'bottom' ? '100%' : 'auto',
+      '--bottom': props.position === 'top' ? '100%' : 'auto',
+      '--left': props.position === 'right' ? '100%' : 'auto',
+      '--right': props.position === 'left' ? '100%' : 'auto',
+    }"
+  >
     <div class="btn">
       <slot />
     </div>
     <template v-if="props.show">
-      <div
-        class="list" :style="{
-          top: props.position === 'bottom' ? '100%' : 'auto',
-          bottom: props.position === 'top' ? '100%' : 'auto',
-          left: props.position === 'right' ? '100%' : 'auto',
-          right: props.position === 'left' ? '100%' : 'auto',
-        }"
-      >
+      <div class="list">
         <div class="box">
-          <slot name="info" />
+          <div class="menu">
+            <template v-for="(i, idx) in list" :key="idx">
+              <div v-if="idx !== 0" class="line" />
+              <div class="item" @click="emits('click', i.name)">
+                {{ i.label }}
+              </div>
+            </template>
+          </div>
         </div>
       </div>
     </template>
@@ -45,10 +52,45 @@ watchEffect(() => {
 
   .list {
     position: absolute;
-    z-index: 100;
 
-    .box{
+    --offset: 20%;
 
+    top: calc(var(--top) + var(--offset));
+    bottom: calc(var(--bottom) + var(--offset));
+    left: calc(var(--left) + var(--offset));
+    right: calc(var(--right) + var(--offset));
+
+    z-index: 90;
+
+    .menu {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      width: 200%;
+      border-radius: 8rpx;
+      border: 2rpx solid #bdbdbd;
+
+      backdrop-filter: blur(5px);
+      background-color: rgba(255, 255, 255, 0.2);
+
+      .item {
+        all: unset;
+        display: flex;
+        justify-content: center;
+
+        font-size: 28rpx;
+
+        width: 60%;
+        padding: 24rpx 32rpx;
+
+      }
+
+      .line {
+        width: 80%;
+        height: 2rpx;
+        background-image: linear-gradient(134deg, rgba(190, 190, 190, 0.2), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.8), rgba(190, 190, 190, 0.2));
+      }
     }
   }
 }
