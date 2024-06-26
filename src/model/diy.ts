b067c7b961e3ad0f69329b33f0caaf2f33856ba1
sampson762 <1769301207@qq.com>
2024-06-26 16:02:16 +0800

    feat: 增加置空和 筛选项列表接口

--- src/model/diy.ts
+++ src/model/diy.ts
@@ -11,9 +11,13 @@ export const useDiyStore = defineStore('diy', {
 
   },
   actions: {
+    // 制空数据列表
+    setModificationListNull() {
+      this.ModificationList = []
+    },
     // 获取游戏列表
     async getGamesList() {
-      const { code, data } = await http.post<gamesList[]>('/web/game/list', { page: 1, pageSize: 10 }, { auth: true })
+      const { code, data } = await http.post<gamesList[]>('/web/game/list', { page: 1, pageSize: 10 })
 
       if (code === 200) {
         this.gamesList = data
@@ -22,33 +26,39 @@ export const useDiyStore = defineStore('diy', {
     },
     // 获取游戏性能
     async getGamePower(params: powerParams) {
-      const { code, data } = await http.post<FpsPowerRes[]>('/web/game/performance/list', params, { auth: true })
+      const { code, data } = await http.post<FpsPowerRes[]>('/web/game/performance/list', params)
       if (code === 200) {
         return data
       }
     },
-    // 获取改配的列表
+    // 获取改配的列表`
     async getModificationList(params: Modification) {
-      const { code, data } = await http.post('/web/product/list/by/params', params, { auth: true })
+      const { code, data } = await http.post('/web/product/list/by/params', params)
       if (code === 200) {
-        return this.ModificationList = [...this.ModificationList, ...data]
+        this.ModificationList = [...this.ModificationList, ...data]
       }
     },
+
     // 获取配置单列表
     async configurationList(params: configurationListReq) {
-      await http.post('/web/product/config/list', params, { auth: true })
+      await http.post('/web/product/config/list', params)
     },
 
-    // 新增配置单 /product/config/add
+    // 新增配置单
     async addConfiguration(params: addConfiguration) {
-      await http.post('/web/product/config/add', params, { auth: true })
+      await http.post('/web/product/config/add', params)
     },
     // 下单
     async submit(params: submitReq) {
-      await http.post('/web/order/add', params, { auth: true })
+      await http.post('/web/order/add', params)
+    },
+
+    // 获取配置的筛选条件
+    async getFilter() {
+      await http.post('/web/product/type/param/list', { ids: [2000] })
     },
 
   },
 
-  persist: true,
+  // persist: true,
 })
