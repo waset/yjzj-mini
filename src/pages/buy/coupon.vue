9ca3fdc1eb9f817a2c9d0e16c7f7e4f98e784ec8
sampson762 <1769301207@qq.com>
2024-06-28 16:14:44 +0800

    feat: 优惠券添加兑换功能v

--- src/pages/buy/coupon.vue
+++ src/pages/buy/coupon.vue
@@ -2,46 +2,74 @@
 const { couponList } = storeToRefs(useSubmitOrderStore())
 const { getCouponList } = useSubmitOrderStore()
 const page = ref<number>(1)
+
 onShow(async () => {
+  page.value = 0
   try {
     // 获取优惠列表
-    await getCouponList(page.value, 15)
+    await getCouponList(page.value += 1, 15)
   }
   catch (error) {
     console.error('Failed to fetch coupon list:', error)
   }
 })
+
+const show = ref<boolean>(false)
+const rule = ref('')
+const showrule = (index: number) => {
+  if (show.value) {
+    return show.value = false
+  }
+  rule.value = couponList.value[index].ticketInfo.instruction || ''
+  show.value = true
+}
+
+// 更新优惠券列表
+const updateCouponFn = async () => {
+  page.value = 0
+  await getCouponList(page.value += 1, 15)
+}
+// 触底加载更多
+onReachBottom(async () => {
+  await getCouponList(page.value += 1, 15)
+})
 </script>
 
 <template>
-  <navbar-back text="我的优惠券" />
-  <div class="body">
-    <buys-coupon-search />
-    <div v-if="couponList.length === 0">
-      <common-empty text="当前暂无优惠券" icon="i-svg-nocoupon" />
+  <div class="">
+    <navbar-back text="我的优惠券" />
+    <div class="body">
+      <buys-coupon-search @update-coupon="updateCouponFn" />
+
+      <template v-if="couponList.length === 0">
+        <common-empty text="当前暂无优惠券" icon="i-svg-nocoupon" />
+      </template>
+      <buys-coupon-item :coupn="couponList" :is-select="true" @click="showrule" />
     </div>
-    <div v-for="(item, index) in couponList" :key="index">
-      <buys-coupon-item :coupn="item" :is-select="true" />
-    </div>
+    <common-popup v-model:show="show" name="规则说明">
+      <div class="h-full text-center lh-[40rpx]">
+        {{ rule }}
+      </div>
+    </common-popup>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  $top-height: 112rpx;
-  $bottom-height: 156rpx;
-  $Be: #BEBEBE;
+$top-height: 112rpx;
+$bottom-height: 156rpx;
+$Be: #BEBEBE;
 
-  .i-background-product-detail {
-    width: 100%;
-    height: 100%;
-  }
+.i-background-product-detail {
+  width: 100%;
+  height: 100%;
+}
 
-  .body {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    overflow: hidden;
-  }
+.body {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow: hidden;
+}
 </style>
 <!--
 <route lang="json">
