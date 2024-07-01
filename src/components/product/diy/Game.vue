860e7f4362812ad93d3314ea7d45fccaf6a7f937
sampson762 <1769301207@qq.com>
2024-07-01 16:17:32 +0800

    fix: 修改 游戏列表中的克隆方式

--- src/components/product/diy/Game.vue
+++ src/components/product/diy/Game.vue
@@ -46,7 +46,7 @@ const confirmGames = () => {
 }
 
 // 选择操作
-const selectAction = (item: gamesList) => {
+const selectAction = (item: gamesList, i: number) => {
   const index = assignment.value.findIndex((obj) => {
     return obj.id === item.id
   })
@@ -54,7 +54,7 @@ const selectAction = (item: gamesList) => {
     assignment.value.splice(index, 1)
   }
   else {
-    assignment.value.push(item)
+    assignment.value.splice(i, 0, item)
   }
 }
 
@@ -75,7 +75,8 @@ onMounted(async () => {
    * TODO:@sampson762
    * @see https://vueuse.org/core/useCloned/
    */
-  assignment.value = JSON.parse(JSON.stringify(gemelist))
+  const { cloned } = useCloned(gemelist)
+  assignment.value = cloned.value
 })
 
 onUnmounted(() => {
@@ -104,7 +105,7 @@ defineExpose({
           </div>
           <div class="selectbox">
             <div v-for="(item, index) in gamesList" :key="index">
-              <div class="select" @click="selectAction(item)">
+              <div class="select" @click="selectAction(item, index)">
                 <div v-if="isIncludes(item.id)" class="selected">
                   <div class="icon i-icons-correct" />
                 </div>
@@ -130,110 +131,110 @@ defineExpose({
 </template>
 
 <style scoped lang="scss">
-  .game {
+.game {
 
-    .content {
-      padding: 32rpx 22rpx;
-      border-radius: 32rpx;
-      background-size: cover;
-      background-position: center center;
-      background-repeat: no-repeat;
-      background-image: var(--bg-img);
+  .content {
+    padding: 32rpx 22rpx;
+    border-radius: 32rpx;
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-image: var(--bg-img);
+  }
+
+  .selectGames {
+    .search {
+      display: flex;
+      height: 88rpx;
+      justify-content: space-between;
+      align-items: center;
+      border: 1rpx solid #A7F522;
+      border-radius: 44rpx;
+      padding: 0 24rpx;
+      margin-bottom: 32rpx;
     }
 
-    .selectGames {
-      .search {
-        display: flex;
-        height: 88rpx;
-        justify-content: space-between;
-        align-items: center;
-        border: 1rpx solid #A7F522;
-        border-radius: 44rpx;
-        padding: 0 24rpx;
-        margin-bottom: 32rpx;
-      }
+    .selectbox {
+      padding-bottom: 120rpx;
+    }
 
-      .selectbox {
-        padding-bottom: 120rpx;
-      }
+    .select {
+      position: relative;
+      border-radius: 16rpx;
+      height: 140rpx;
+      background-color: rgba(#000, 0.2);
+      padding: 20rpx;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 32rpx;
+      z-index: 9;
 
-      .select {
-        position: relative;
-        border-radius: 16rpx;
-        height: 140rpx;
-        background-color: rgba(#000, 0.2);
-        padding: 20rpx;
-        box-sizing: border-box;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 32rpx;
-        z-index: 9;
+      .selected {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 69rpx;
+        height: 69rpx;
+        background-color: #A7F522;
+        clip-path: polygon(0% 98.571%, 98.571% 0%, 22.857% 0%, 22.857% 0%, 19.15% 0.299%, 15.633% 1.165%, 12.353% 2.551%, 9.358% 4.41%, 6.695% 6.695%, 4.41% 9.358%, 2.551% 12.353%, 1.165% 15.633%, 0.299% 19.15%, 0% 22.857%, 0% 98.571%);
 
-        .selected {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 69rpx;
-          height: 69rpx;
-          background-color: #A7F522;
-          clip-path: polygon(0% 98.571%, 98.571% 0%, 22.857% 0%, 22.857% 0%, 19.15% 0.299%, 15.633% 1.165%, 12.353% 2.551%, 9.358% 4.41%, 6.695% 6.695%, 4.41% 9.358%, 2.551% 12.353%, 1.165% 15.633%, 0.299% 19.15%, 0% 22.857%, 0% 98.571%);
-
-          .icon {
-            color: #000;
-          }
+        .icon {
+          color: #000;
         }
+      }
 
-        .goodsImg {
+      .goodsImg {
+        width: 100rpx;
+        height: 100rpx;
+        border-radius: 16rpx;
+        background-color: #fff;
+        overflow: hidden;
+
+        .img {
           width: 100rpx;
           height: 100rpx;
-          border-radius: 16rpx;
-          background-color: #fff;
-          overflow: hidden;
-
-          .img {
-            width: 100rpx;
-            height: 100rpx;
-          }
-        }
-
-        .goodsInfo {
-          flex: 1;
-
-          .name {
-            margin-left: 48rpx;
-            width: fit-content;
-            width: -webkit-fit-content;
-            width: -moz-fit-content;
-            font-size: 28rpx;
-            font-weight: 600;
-            padding: 0 24rpx;
-            background: linear-gradient(135deg, rgba(#BEBEBE, .1), rgba(#BEBEBE, .4), rgba(#BEBEBE, .4), rgba(#BEBEBE, .1));
-          }
         }
       }
 
-      .confirm {
-        width: 440rpx;
-        height: 80rpx;
-        position: fixed;
-        bottom: calc(32rpx + env(safe-area-inset-bottom));
-        left: 50%;
-        transform: translateX(-50%);
-        padding: 10px 20px 10px 20px;
-        border-radius: 4rpx;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #A7F522;
-        color: #1D2129;
-        font-size: 28rpx;
-        z-index: 99;
-      }
+      .goodsInfo {
+        flex: 1;
 
-      .confirm:active {
-        background-color: #82c410;
+        .name {
+          margin-left: 48rpx;
+          width: fit-content;
+          width: -webkit-fit-content;
+          width: -moz-fit-content;
+          font-size: 28rpx;
+          font-weight: 600;
+          padding: 0 24rpx;
+          background: linear-gradient(135deg, rgba(#BEBEBE, .1), rgba(#BEBEBE, .4), rgba(#BEBEBE, .4), rgba(#BEBEBE, .1));
+        }
       }
     }
+
+    .confirm {
+      width: 440rpx;
+      height: 80rpx;
+      position: fixed;
+      bottom: calc(32rpx + env(safe-area-inset-bottom));
+      left: 50%;
+      transform: translateX(-50%);
+      padding: 10px 20px 10px 20px;
+      border-radius: 4rpx;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #A7F522;
+      color: #1D2129;
+      font-size: 28rpx;
+      z-index: 99;
+    }
+
+    .confirm:active {
+      background-color: #82c410;
+    }
   }
+}
 </style>
