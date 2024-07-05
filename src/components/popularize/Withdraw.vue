a205d6359fb1ccc84a7ecf8ffec07593f95793ed
XieJingao <496929151@qq.com>
2024-07-05 15:55:50 +0800

    feat: 发起提现功能

--- src/components/popularize/Withdraw.vue
+++ src/components/popularize/Withdraw.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-const { userDesc } = storeToRefs(useUserStore())
-const showWIthdraw = ref(false) // 提现表单弹出
-const showModel = ref(false) // 提现成功提示
+import { allowedNodeEnvironmentFlags } from 'node:process'
 
-// 账户信息
+// 提现成功提示
+const { userDesc, user } = storeToRefs(useUserStore())
+const { CashWithdraw } = usePopularizeStore()
+const showWIthdraw = ref(false) // 提现表单弹出
+const showModel = ref(false)// 账户信息
 const moneyObject = ref({
   proportion: {
     title: '返利比例',
@@ -15,7 +17,7 @@ const moneyObject = ref({
   },
   balance: {
     title: '账号余额',
-    value: Number.parseFloat(userDesc.value.balanceAmount).toFixed(2),
+    value: Number.parseFloat(userDesc.value.balanceAmount).toFixed(2) + 1,
   },
 })
 // 提现说明
@@ -30,10 +32,53 @@ function eventWithdraw() {
     showWIthdraw.value = !showWIthdraw.value
   }
 }
+
+// 提现金额
+const amount = ref()
+
+// 全部提现
+function allWithdraw() {
+  amount.value = user.value.balanceAmount
+}
+
 // 提现确认
-function withdrawEvent() {
-  showWIthdraw.value = false
-  showModel.value = true
+async function withdrawEvent() {
+  if (!amount.value || amount.value <= 0) {
+    return uni.showToast({ title: '请正确输入提现金额', icon: 'none' })
+  }
+
+  if (user.value.isSub === 2) {
+    uni.showToast({ title: '请先关注“一剑装机”公众号', icon: 'none' })
+    return
+  }
+
+  if (amount.value > 2000) {
+    uni.showToast({ title: '单笔提现金额不能超过2000元', icon: 'none' })
+    return
+  }
+
+  if (amount.value < 10) {
+    uni.showToast({ title: '单笔提现金额不能少于10元', icon: 'none' })
+    return
+  }
+
+  const res = await CashWithdraw({ applyAmount: amount.value })
+  if (res.code === 200) {
+    showWIthdraw.value = false
+    showModel.value = true
+  }
+  else {
+    uni.showToast({ title: res.msg, icon: 'none' })
+  }
+}
+
+// 输入金额触发 计算可提现金额
+const realAmount = ref()
+const upinp = useDebounceFn((e) => {
+  realAmount.value = e
+}, 200)
+function InpAmount(e: any) {
+  upinp(e.target.value)
 }
 </script>
 
@@ -84,7 +129,7 @@ function withdrawEvent() {
           ￥
         </div>
         <div class="money_two">
-          100.00
+          {{ user.balanceAmount }}
         </div>
       </div>
       <div class="money_way">
@@ -107,19 +152,21 @@ function withdrawEvent() {
       提现金额
     </div>
     <div class="input_view">
-      <input type="number" class="sum_input" placeholder="输入您想提现的金额">
+      <input v-model="amount" type="number" class="sum_input" placeholder="输入您想提现的金额" @input="InpAmount">
       <div class="money_icon">
         ￥
       </div>
-      <div class="sum_all">
+      <div class="sum_all" @click="allWithdraw">
         全部提现
       </div>
     </div>
     <!-- 渐变底线 -->
     <div class="gradient-line" />
-    <div class="reality_sum">
-      实际到账金额 66.00
-    </div>
+    <template v-if="realAmount">
+      <div class="reality_sum">
+        实际到账金额 {{ realAmount }}
+      </div>
+    </template>
     <div class="withdraw_explain" style="margin-top: 60rpx;">
       <div class="icon i-icons-info" style="margin-right: 6.66rpx;" /> 提现说明
     </div>
@@ -134,253 +181,253 @@ function withdrawEvent() {
 
 <style lang="scss" scoped>
 // 推广信息
-  .popu_user {
-    width: calc(100% - 64rpx);
-    height: 300rpx;
-    margin: 0 auto;
-    margin-top: 119rpx;
-    border-radius: 32rpx;
+.popu_user {
+  width: calc(100% - 64rpx);
+  height: 300rpx;
+  margin: 0 auto;
+  margin-top: 119rpx;
+  border-radius: 32rpx;
+  overflow: hidden;
+  padding: 0px 0px 32rpx 0px;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), linear-gradient(0deg, rgba(132, 132, 132, 0.2), rgba(132, 132, 132, 0.2));
+
+  .user {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    height: 80rpx;
+    padding: 0rpx 32rpx;
+    box-sizing: border-box;
+    line-height: 80rpx;
     overflow: hidden;
-    padding: 0px 0px 32rpx 0px;
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), linear-gradient(0deg, rgba(132, 132, 132, 0.2), rgba(132, 132, 132, 0.2));
+    background: linear-gradient(90deg, rgba(109, 109, 109, 0) 1.52%, rgba(202, 202, 202, 0.5) 53.11%, rgba(109, 109, 109, 0) 98.86%);
 
-    .user {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-
-      height: 80rpx;
-      padding: 0rpx 32rpx;
-      box-sizing: border-box;
+    .grade {
+      font-size: 25rpx;
       line-height: 80rpx;
-      overflow: hidden;
-      background: linear-gradient(90deg, rgba(109, 109, 109, 0) 1.52%, rgba(202, 202, 202, 0.5) 53.11%, rgba(109, 109, 109, 0) 98.86%);
-
-      .grade {
-        font-size: 25rpx;
-        line-height: 80rpx;
-      }
-
-      .popu_svg_medal {
-        width: 38.78rpx;
-        height: 31.46rpx;
-      }
-
-      .integral_right {
-        line-height: 80rpx;
-        font-size: 24rpx;
-        font-weight: 500;
-        color: rgba(255, 223, 109, 1);
-      }
     }
 
-    .money_view {
-      height: 220rpx;
-      overflow: hidden;
+    .popu_svg_medal {
+      width: 38.78rpx;
+      height: 31.46rpx;
+    }
 
-      .wrap {
-        margin-top: 32rpx;
-        @apply flex-around;
+    .integral_right {
+      line-height: 80rpx;
+      font-size: 24rpx;
+      font-weight: 500;
+      color: rgba(255, 223, 109, 1);
+    }
+  }
 
-        .money_details {
+  .money_view {
+    height: 220rpx;
+    overflow: hidden;
+
+    .wrap {
+      margin-top: 32rpx;
+      @apply flex-around;
+
+      .money_details {
+        text-align: center;
+
+        .money_title {
+          font-family: PingFang SC;
+          font-size: 24rpx;
+          font-weight: 400;
+          line-height: 40rpx;
           text-align: center;
+          margin-bottom: 8rpx;
+        }
 
-          .money_title {
-            font-family: PingFang SC;
-            font-size: 24rpx;
-            font-weight: 400;
-            line-height: 40rpx;
-            text-align: center;
-            margin-bottom: 8rpx;
-          }
-
-          .subhead {
-            font-family: PingFang SC;
-            font-size: 32rpx;
-            font-weight: 600;
-            line-height: 40rpx;
-            text-align: center;
-            color: #a7f522;
-          }
+        .subhead {
+          font-family: PingFang SC;
+          font-size: 32rpx;
+          font-weight: 600;
+          line-height: 40rpx;
+          text-align: center;
+          color: #a7f522;
         }
       }
-
-      .subheads {
-        font-family: PingFang SC;
-        font-size: 28rpx;
-        text-align: center;
-        color: #a7f522;
-        text-align: right;
-        margin-top: 24rpx;
-        padding-right: 60rpx;
-        box-sizing: border-box;
-        @apply underline underline-offset-3;
-      }
-
-      .withdraw {
-        @apply underline underline-offset-3;
-        font-size: 28rpx;
-        text-align: right;
-        margin-top: 24rpx;
-        padding-right: 60rpx;
-        box-sizing: border-box;
-        color: #8d8d8d;
-      }
-    }
-  }
-
-  .money_view_popup {
-    height: 236rpx;
-    margin: 0 auto;
-    border-radius: 36rpx;
-    overflow: hidden;
-    position: relative;
-    background: linear-gradient(277.69deg, rgba(21, 21, 21, 0.9) 39.19%, rgba(74, 74, 74, 0.9) 94.91%);
-
-    .icon {
-      width: 240rpx;
-      height: 240rpx;
-      position: absolute;
-      right: 0rpx;
-      bottom: -25rpx;
-      opacity: 0.5;
     }
 
-    .money_text {
+    .subheads {
       font-family: PingFang SC;
       font-size: 28rpx;
-      font-weight: 400;
-      line-height: 40rpx;
-      text-align: left;
-      padding: 32rpx 0rpx 0rpx 32rpx;
-      box-sizing: border-box;
-    }
-
-    .money {
-      font-size: 28rpx;
-      line-height: 40rpx;
-      text-align: left;
-      color: rgba(167, 245, 34, 1);
-      padding: 24rpx 0rpx 0rpx 32rpx;
-      box-sizing: border-box;
-
-      .money_one {
-        font-size: 28rpx;
-        display: inline-block;
-      }
-
-      .money_two {
-        display: inline-block;
-        font-size: 34rpx;
-      }
-    }
-
-    .money_way {
-      text-align: right;
-      font-size: 28rpx;
-      font-weight: 400;
-      padding-right: 32rpx;
-      position: absolute;
-      margin-top: 32rpx;
-      right: 0;
-      z-index: 2;
-
-      .money_way_svg {
-        width: 32rpx;
-        height: 32rpx;
-        display: inline-block;
-        z-index: 2;
-        margin-left: 25rpx;
-        transform: translate(6rpx, -2rpx);
-      }
-    }
-  }
-
-  .ensure_view {
-    @apply flex-between;
-    width: 100%;
-    padding: 24rpx 32rpx;
-    border-radius: 8rpx;
-    background: linear-gradient(100.04deg, #000000 0.82%, rgba(0, 0, 0, 0.5) 49.3%, #000000 91.18%);
-    margin-top: 48rpx;
-    font-size: 28rpx !important;
-
-    .ensure_style {
-      .money_way_svg {
-        width: 32rpx;
-        height: 32rpx;
-        display: inline-block;
-        transform: translate(6rpx, -2rpx);
-      }
-    }
-  }
-
-  .popup_text {
-    font-size: 28rpx;
-    margin-top: 48rpx;
-  }
-
-  .input_view {
-    width: 100%;
-    margin-top: 16rpx;
-    position: relative;
-    padding: 20rpx 200rpx 20rpx 50rpx;
-    box-sizing: border-box;
-
-    .sum_input {
-      width: 100%;
-      height: 40rpx;
-    }
-
-    .money_icon {
-      position: absolute;
-      left: 0;
-      top: 50%;
-      font-size: 40rpx;
-      transform: translateY(-50%);
-    }
-
-    .sum_all {
-      position: absolute;
-      right: 0;
-      top: 50%;
-      transform: translateY(-50%);
-      font-size: 28rpx;
+      text-align: center;
       color: #a7f522;
+      text-align: right;
+      margin-top: 24rpx;
+      padding-right: 60rpx;
+      box-sizing: border-box;
       @apply underline underline-offset-3;
     }
+
+    .withdraw {
+      @apply underline underline-offset-3;
+      font-size: 28rpx;
+      text-align: right;
+      margin-top: 24rpx;
+      padding-right: 60rpx;
+      box-sizing: border-box;
+      color: #8d8d8d;
+    }
+  }
+}
+
+.money_view_popup {
+  height: 236rpx;
+  margin: 0 auto;
+  border-radius: 36rpx;
+  overflow: hidden;
+  position: relative;
+  background: linear-gradient(277.69deg, rgba(21, 21, 21, 0.9) 39.19%, rgba(74, 74, 74, 0.9) 94.91%);
+
+  .icon {
+    width: 240rpx;
+    height: 240rpx;
+    position: absolute;
+    right: 0rpx;
+    bottom: -25rpx;
+    opacity: 0.5;
   }
 
-  .gradient-line {
-    border-bottom: 3rpx solid transparent;
-    background: linear-gradient(98.8deg, rgba(190, 190, 190, 0.1) 5.58%, rgba(190, 190, 190, 0.4) 35.3%, rgba(190, 190, 190, 0.4) 65.93%, rgba(190, 190, 190, 0.1) 92.04%) border-box;
-    mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
-    mask-composite: exclude;
-  }
-
-  .reality_sum {
-    margin-top: 8rpx;
+  .money_text {
+    font-family: PingFang SC;
     font-size: 28rpx;
-    color: rgba(255, 125, 0, 1);
-  }
-
-  .withdraw_explain {
-    width: calc(100% - 42rpx);
-    margin: 0 auto;
-    font-size: 24rpx;
+    font-weight: 400;
     line-height: 40rpx;
-    margin-bottom: 24rpx;
+    text-align: left;
+    padding: 32rpx 0rpx 0rpx 32rpx;
+    box-sizing: border-box;
   }
 
-  .button_style {
-    width: 440rpx;
-    height: 80rpx;
-    padding: 20rpx 40rpx 20rpx 40rpx;
-    border-radius: 4rpx;
-    color: #000;
-    margin: 0 auto;
-    margin-top: 68rpx;
-    text-align: center;
-    background: rgba(167, 245, 34, 1);
+  .money {
+    font-size: 28rpx;
+    line-height: 40rpx;
+    text-align: left;
+    color: rgba(167, 245, 34, 1);
+    padding: 24rpx 0rpx 0rpx 32rpx;
+    box-sizing: border-box;
+
+    .money_one {
+      font-size: 28rpx;
+      display: inline-block;
+    }
+
+    .money_two {
+      display: inline-block;
+      font-size: 34rpx;
+    }
   }
+
+  .money_way {
+    text-align: right;
+    font-size: 28rpx;
+    font-weight: 400;
+    padding-right: 32rpx;
+    position: absolute;
+    margin-top: 32rpx;
+    right: 0;
+    z-index: 2;
+
+    .money_way_svg {
+      width: 32rpx;
+      height: 32rpx;
+      display: inline-block;
+      z-index: 2;
+      margin-left: 25rpx;
+      transform: translate(6rpx, -2rpx);
+    }
+  }
+}
+
+.ensure_view {
+  @apply flex-between;
+  width: 100%;
+  padding: 24rpx 32rpx;
+  border-radius: 8rpx;
+  background: linear-gradient(100.04deg, #000000 0.82%, rgba(0, 0, 0, 0.5) 49.3%, #000000 91.18%);
+  margin-top: 48rpx;
+  font-size: 28rpx !important;
+
+  .ensure_style {
+    .money_way_svg {
+      width: 32rpx;
+      height: 32rpx;
+      display: inline-block;
+      transform: translate(6rpx, -2rpx);
+    }
+  }
+}
+
+.popup_text {
+  font-size: 28rpx;
+  margin-top: 48rpx;
+}
+
+.input_view {
+  width: 100%;
+  margin-top: 16rpx;
+  position: relative;
+  padding: 20rpx 200rpx 20rpx 50rpx;
+  box-sizing: border-box;
+
+  .sum_input {
+    width: 100%;
+    height: 40rpx;
+  }
+
+  .money_icon {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    font-size: 40rpx;
+    transform: translateY(-50%);
+  }
+
+  .sum_all {
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 28rpx;
+    color: #a7f522;
+    @apply underline underline-offset-3;
+  }
+}
+
+.gradient-line {
+  border-bottom: 3rpx solid transparent;
+  background: linear-gradient(98.8deg, rgba(190, 190, 190, 0.1) 5.58%, rgba(190, 190, 190, 0.4) 35.3%, rgba(190, 190, 190, 0.4) 65.93%, rgba(190, 190, 190, 0.1) 92.04%) border-box;
+  mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
+  mask-composite: exclude;
+}
+
+.reality_sum {
+  margin-top: 8rpx;
+  font-size: 28rpx;
+  color: rgba(255, 125, 0, 1);
+}
+
+.withdraw_explain {
+  width: calc(100% - 42rpx);
+  margin: 0 auto;
+  font-size: 24rpx;
+  line-height: 40rpx;
+  margin-bottom: 24rpx;
+}
+
+.button_style {
+  width: 440rpx;
+  height: 80rpx;
+  padding: 20rpx 40rpx 20rpx 40rpx;
+  border-radius: 4rpx;
+  color: #000;
+  margin: 0 auto;
+  margin-top: 68rpx;
+  text-align: center;
+  background: rgba(167, 245, 34, 1);
+}
 </style>
