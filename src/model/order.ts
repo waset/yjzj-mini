9d2b6b3987d3b083757a144e5ff9d55469a8100d
LdddLi <1653243099@qq.com>
2024-07-05 13:35:49 +0800

    feat: 加入分页

--- src/model/order.ts
+++ src/model/order.ts
@@ -1,24 +1,34 @@
 export const useOrderStore = defineStore('orders', {
   state: (): {
     orders: Order[]
+    lastPage: number
     express: Express
   } => ({
     orders: [] as Order[],
+    lastPage: 1,
     express: {} as Express,
   }),
 
   actions: {
     // 获取订单状态
-    async getOrderList(status: Order['status'], page: number = 1, pageSize: number = 1000) {
+    async getOrderList(status: number, Page: number, pageSize: number) {
       const sta = status !== undefined && status !== 0 ? Number(status) : null
-      const { data, code } = await http.post<Order[]>('/web/order/list', {
+      const { data, code, page } = await http.post<OrderRes>('/web/order/list', {
         status: sta,
-        page,
+        Page,
         pageSize,
       }, { auth: true })
 
       if (code === 200) {
-        this.orders = data
+        if (Page === 1) {
+          // 如果是第一页，直接替换数据
+          this.orders = data
+        }
+        else {
+          // 如果不是第一页，追加数据
+          this.orders = this.orders.concat(data)
+        }
+        this.lastPage = page?.lastPage || 1
       }
     },
 
