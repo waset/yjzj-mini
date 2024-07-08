d3f385cca178699bb2a0615f9f0db28a8183a525
LdddLi <1653243099@qq.com>
2024-07-08 17:06:27 +0800

    chore: 增加非diy商品的产品信息

--- src/pages/product/detail.vue
+++ src/pages/product/detail.vue
@@ -76,6 +76,28 @@ onShareAppMessage(() => {
       <product-banner :list="detail?.banner || []" />
     </div>
 
+    <div class="top">
+      <div class="wrap">
+        <div class="title">
+          {{ detail?.name }}
+        </div>
+        <div class="desc">
+          {{ detail?.description }}
+        </div>
+        <div class="more">
+          <div class="price">
+            <span>￥</span>
+            <span>{{ detail?.sellPrice || 0 }}</span>
+          </div>
+          <div class="btns">
+            <div class="btn">
+              <div class="i-icons-share" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="info">
       <product-info :info="detail" />
     </div>
@@ -92,6 +114,44 @@ onShareAppMessage(() => {
 
   .banner {
     padding: 0 32rpx;
+  }
+
+  .top {
+    padding: 32rpx;
+
+    .wrap {
+      padding: 32rpx;
+      border-radius: 16rpx;
+      background-color: #000;
+      word-break: break-all;
+
+      .title {
+        font-size: 36rpx;
+        line-height: 48rpx;
+        color: rgba(245, 245, 245, 1);
+      }
+
+      .desc {
+        font-size: 24rpx;
+        line-height: 36rpx;
+        color: #bebebe;
+        padding: 16rpx 0;
+      }
+
+      .more {
+        @apply flex-between;
+        font-size: 48rpx;
+        line-height: 56rpx;
+
+        .price {
+          @apply text-green;
+        }
+      }
+    }
+
+    .price {
+      @apply text-green;
+    }
   }
 
 }
