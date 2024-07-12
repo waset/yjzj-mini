f88c9001ee10bfcd7acbc151349197d8de4d41c1
glooe <93868134+XieJingao@users.noreply.github.com>
2024-07-12 17:25:07 +0800

    feat: 分享邀请推广页并绑定该分享者 (#88)

--- src/pages/popularize/invitetion.vue
+++ src/pages/popularize/invitetion.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 const { levelall, inviterank, inviteuser } = storeToRefs(useInviteStore())
-const { getInviteall, getInviteRank, becomePromoter } = useInviteStore()
-const { user } = storeToRefs(useUserStore())
+const { getInviteall, getInviteRank, becomePromoter, bandInviter } = useInviteStore()
+const { user, shareCode } = storeToRefs(useUserStore())
+const { getUserInfo } = useUserStore()
 onShow(async () => {
   await getInviteall()
   await getInviteRank()
@@ -36,6 +37,82 @@ async function become() {
     })
   }
 }
+
+// 确认换绑分享人弹窗显隐
+const showModel = ref(false)
+// 该分享邀请人邀请码
+const shareCodeInner = ref('')
+// 绑定邀请人
+async function bindCode(code: string) {
+  const res = await bandInviter(code)
+  if (res.code === 200) {
+    uni.showToast({
+      title: '绑定成功',
+      icon: 'none',
+    })
+    await getUserInfo()
+  }
+  else {
+    uni.showToast({
+      title: res.msg,
+      icon: 'none',
+    })
+  }
+}
+async function bindShareCode(code: string) {
+  // 登录且没有加入邀请人计划
+  if (user.value.phone && user.value.promoterStatus !== 1) {
+    await bindCode(code)
+  }
+  // 登录且加入邀请人计划
+  if (user.value.phone && user.value.promoterStatus === 1) {
+    // 分享者不是自己和已绑定的邀请人
+    if (code !== user.value.inviteCode)
+      showModel.value = true
+  }
+  // 没登陆
+  if (!user.value.phone) {
+    // 存储分享者邀请码
+    shareCode.value = code
+    uni.showToast({
+      title: '请先登录',
+      icon: 'none',
+    })
+    setTimeout(() => {
+      Jump('/pages/me/login')
+    }, 1000)
+  }
+}
+// 换绑分享人的邀请码
+async function ChangeBind() {
+  await bindCode(shareCodeInner.value)
+  showModel.value = false
+}
+
+onLoad((params) => {
+  if (params?.inviteCode) {
+    shareCode.value = params?.inviteCode
+    shareCodeInner.value = params.inviteCode
+    bindShareCode(params.inviteCode)
+  }
+})
+
+// #ifdef MP
+onShareAppMessage(() => {
+  if (!user.value.phone) {
+    uni.showToast({
+      title: '请先登录',
+      icon: 'none',
+    })
+  }
+  // 如果是邀请人 则分享时携带邀请码
+  const params = !user.value.inviteCode ? {} : { inviteCode: user.value.inviteCode }
+  return {
+    title: '快来加入推广人计划吧！',
+    path: UrlAndParams('/pages/popularize/invitetion', params),
+  }
+})
+// #endif
 </script>
 
 <template>
@@ -62,6 +139,7 @@ async function become() {
       </div>
     </div>
     <popularize-bind-inviter ref="childRef" :user="user" />
+    <common-model v-model:show="showModel" msg="确认邀请人换绑为该分享者" icon="i-svg-warn" @ok="ChangeBind" />
   </div>
 </template>
 
@@ -74,7 +152,7 @@ async function become() {
 }
 
 .sharebtn {
-  background:none ;
+  background: none;
   @apply flex-center w-full h-full;
 }
 </style>
