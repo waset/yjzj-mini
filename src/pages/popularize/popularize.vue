630533e14ac699fd97f1d683147415a1ff8694bf
XieJingao <496929151@qq.com>
2024-07-30 14:45:51 +0800

    feat: 邀请链接生成二维码并保存

--- src/pages/popularize/popularize.vue
+++ src/pages/popularize/popularize.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import UQRCode from 'uqrcodejs'
+
 const { getUserInfo } = useUserStore()
 const { user } = storeToRefs(useUserStore())
 const { getRebatelist, getWithdrawlist } = usePopularizeStore()
@@ -18,13 +20,68 @@ const childRef = ref<ComponentInstance['PopularizeBindInviter']>()
 function bandinvite() {
   childRef.value?.openShow()
 }
+
+function save() {
+  uni.canvasToTempFilePath({
+    canvasId: 'qrcode',
+    width: 200,
+    height: 200,
+    x: -10,
+    y: -10,
+
+    success: (res) => {
+      if (res?.tempFilePath) {
+        uni.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success: () => {
+            uni.showToast({
+              title: '保存成功',
+              icon: 'success',
+              duration: 2000,
+            })
+          },
+          fail: () => {
+            uni.showToast({
+              title: '保存失败',
+              icon: 'error',
+              duration: 2000,
+            })
+          },
+        })
+      }
+    },
+  })
+}
+
+onMounted(() => {
+  // 获取uQRCode实例
+  const qr = new UQRCode()
+  // 设置二维码内容
+  const url = MobileUrl('/login?inviteCode=') + user.value.inviteCode
+  qr.data = url
+  qr.size = 200
+  qr.background = '#ffffff'
+  qr.x = -10
+  qr.y = -10
+  qr.margin = 10
+  qr.border = false
+  // 调用制作二维码方法
+  qr.make()
+  // 获取canvas上下文
+  const canvasContext: UniApp.CanvasContext = uni.createCanvasContext('qrcode', this) // 如果是组件，this必须传入
+  // 设置uQRCode实例的canvas上下文
+  qr.canvasContext = canvasContext
+  // 调用绘制方法将二维码图案绘制到canvas上
+  qr.drawCanvas()
+})
 </script>
 
 <template>
   <div class="popularize">
     <navbar-back text="邀请推广" />
+    <canvas id="qrcode" class="fixed left-1000" canvas-id="qrcode" style="width: 200px;height: 200px;" />
     <!-- 推广邀请 -->
-    <popularize-info @bandinvite="bandinvite" @openwith="openWithdraw" />
+    <popularize-info @bandinvite="bandinvite" @openwith="openWithdraw" @savecode="save" />
     <!-- 返利记录 & 提现记录 -->
     <popularize-record-list :rebatelist="rebatelist" :withdrawlist="withdrawlist" />
     <!-- 立即提现 & 提现弹窗 -->
